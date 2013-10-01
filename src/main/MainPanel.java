a2e940c15215c2f795ea051d25220093225aac39
Stephen Wen <stephen.c.wen@gmail.com>
2013-10-01 11:51:49 -0400

    added the MainPanel class to host the gui

---
+++ src/main/MainPanel.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+import javax.swing.JPanel;
+/**
+ *
+ * @author owner
+ */
+public class MainPanel extends JPanel{
+    
+}
